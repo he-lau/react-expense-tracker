e5e55ef4771322768c6d9b9cb4cc25684ba008a3
he-lau <he.laurent.1130@gmail.com>
2024-03-28 16:05:12 +0100

    first commit

--- src/containers/ExpenseInput/ExpenseInput.jsx
+++ src/containers/ExpenseInput/ExpenseInput.jsx
@@ -1,15 +1,32 @@
+import { addExpense } from "store/expense/expense-slice";
 import s from "./style.module.css";
+import { useDispatch } from "react-redux";
 
 export function ExpenseInput(props) {
+  // trigger slice event
+  const dispatch = useDispatch();
+
+  function submit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const label = formData.get("label");
+    const price = formData.get("price");
+    console.log(label, price);
+
+    const expense = { label: label, price: price };
+    dispatch(addExpense(expense));
+  }
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
           <input
             type="text"
             className="form-control"
             placeholder='Ex : "Apple"'
-            name="name"
+            name="label"
           />
         </div>
         <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-2">
